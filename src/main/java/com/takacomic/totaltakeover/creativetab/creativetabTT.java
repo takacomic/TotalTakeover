65dfc4f7c89468498e6fef69ef98551896a13445
takacomic <theandrewclong@gmail.com>
2014-08-10 00:57:29 -0500

    Wont commit?

---
+++ src/main/java/com/takacomic/totaltakeover/creativetab/creativetabTT.java
@@ -0,0 +1,17 @@
+package com.takacomic.totaltakeover.creativetab;
+
+import com.takacomic.totaltakeover.reference.Reference;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class creativetabTT
+{
+    public static final CreativeTabs TT_TAB = new CreativeTabs(Reference.MOD_ID.toLowerCase())
+    {
+        @Override
+        public Item getTabIconItem()
+        {
+            return null;
+        }
+    }
+}
