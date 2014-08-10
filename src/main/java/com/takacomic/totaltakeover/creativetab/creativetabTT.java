d4bfc64108080d7cb5c446064179704ba50b9d2f
takacomic <theandrewclong@gmail.com>
2014-08-10 01:11:09 -0500

    Work

--- src/main/java/com/takacomic/totaltakeover/creativetab/creativetabTT.java
+++ src/main/java/com/takacomic/totaltakeover/creativetab/creativetabTT.java
@@ -2,16 +2,17 @@ package com.takacomic.totaltakeover.creativetab;
 
 import com.takacomic.totaltakeover.reference.Reference;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 
 public class CreativetabTT
 {
     public static final CreativeTabs TT_TAB = new CreativeTabs(Reference.MOD_ID.toLowerCase())
     {
-        @Override
+       @Override
         public Item getTabIconItem()
         {
-            return null;
+            return Items.apple;
         }
     };
 }
